95b87513e23336420fc82c5a7042f56bc07cc877
AngelKlasnakov <angelklasnakov83@gmail.com>
2025-01-15 03:25:11 +0200

    main menu functioning buttons

---
+++ SimuDrive/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Load Scenes
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Drive Menu");
+    }
+
+    public void InstructionsButton()
+    {
+        SceneManager.LoadScene("Instructions Menu");
+    }
+
+    public void OptionsButton()
+    {
+        SceneManager.LoadScene("Options Menu");
+    }
+
+    //Quit Game
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("The Player has Quit the game");
+    }
+}
